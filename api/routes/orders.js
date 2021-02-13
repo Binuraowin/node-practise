1473a5e340b0e223063cd1a6f59e134d1239fc6a
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-14 02:22:35 +0530

    Update orders.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,12 +4,34 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Order = require('../models/order')
 
-router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message:'orders/get'
-    })
-})
+router.get("/", (req, res, next) => {
+    Order.find()
+      .select("product quantity _id")
+      .exec()
+      .then(docs => {
+        res.status(200).json({
+          count: docs.length,
+          orders: docs.map(doc => {
+            return {
+              _id: doc._id,
+              product: doc.product,
+              quantity: doc.quantity,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/orders/" + doc._id
+              }
+            };
+          })
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
+   
 router.post('/',(req,res,next)=>{
     const order = new Order({
         _id: new mongoose.Types.ObjectId(),
