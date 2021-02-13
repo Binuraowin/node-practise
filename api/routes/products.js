047dbb40b622574b8535855d9470c144e25c340f
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-13 12:35:36 +0530

    Update products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,9 +4,18 @@ const mongoose = require('mongoose');
 const Product = require('../models/products')
 
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message:'products/get'
+    Product.find().exec().then(docs =>{
+        res.status(200).json(
+            docs
+        )
+        console.log(docs)
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
     })
+    
 })
 
 router.post('/',(req,res,next)=>{  
@@ -32,7 +41,13 @@ router.get('/:productId',(req,res,next)=>{
     const id = req.params.productId;
 
     Product.findById(id).exec().then(doc =>{
-        res.status(200).json(doc);
+        if (doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({
+                message:"Not Found"
+            })
+        }
         console.log(doc)
     }).catch(
         err => {
