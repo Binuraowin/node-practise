1e14f542c810d696cff44c4268e71b5ea40bb8a9
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-15 20:04:20 +0530

    clean

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Order = require('../models/order')
-const Product = require("../models/product");
+
 const checkAuth = require('../middleware/check-auth');
 
 const OrdersController = require('../controllers/orders');
