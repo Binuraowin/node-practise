146179100b4ec1d7d53ac284bcff40dd1294e19d
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-15 18:49:49 +0530

    protect route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Order = require('../models/order')
 const Product = require("../models/product");
+const checkAuth = require('../middleware/check-auth');
 
 
-router.get("/", (req, res, next) => {
+router.get("/",checkAuth, (req, res, next) => {
     Order.find()
       .select("product quantity _id").populate('product')
       .exec()
@@ -34,7 +35,7 @@ router.get("/", (req, res, next) => {
   });
 
    
-router.post('/',(req,res,next)=>{
+router.post('/',checkAuth,(req,res,next)=>{
   Product.findById(req.body.productId).then(product =>{
     if(!product){
       return res.status(404).json({
@@ -67,7 +68,7 @@ router.post('/',(req,res,next)=>{
       });
 })  
 })
-router.get("/:orderId", (req, res, next) => {
+router.get("/:orderId",checkAuth, (req, res, next) => {
   Order.findById(req.params.orderId)
   .populate('product')
     .exec()
@@ -92,7 +93,7 @@ router.get("/:orderId", (req, res, next) => {
     });
 });
 
-router.delete("/:orderId", (req, res, next) => {
+router.delete("/:orderId", checkAuth,(req, res, next) => {
   Order.remove({ _id: req.params.orderId })
     .exec()
     .then(result => {
@@ -112,7 +113,7 @@ router.delete("/:orderId", (req, res, next) => {
     });
 });
 
-router.patch('/:orderId',(req,res,next)=>{
+router.patch('/:orderId',checkAuth,(req,res,next)=>{
     res.status(200).json({
         message:'orders/updated'
     })  
